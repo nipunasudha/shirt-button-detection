e4c90066b3b949d61f28f1220023c8b5ba6fd217
W.H.Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2019-02-20 03:58:55 +0530

    Final detection adjustments

--- detect_buttons.py
+++ detect_buttons.py
@@ -5,30 +5,80 @@ import numpy as np
 import ctypes
 
 # SETTINGS
-PIXELS_PER_INCH = 50.7
-TOLERENCE_INCHES = 0.2
+IMAGE_FILE_NAME = "shirt (2).jpg"
+PIXELS_PER_INCH = 40
+TOLERENCE_INCHES = 0.5
+WIDTH_PERCENTAGE_OF_SCANING_AREA = 30
 
+# DEFINITIONS
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-def get_button_keypoints(grayscale_img):
+def apply_brightness_contrast(input_img, brightness=0, contrast=0):
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness
+        alpha_b = (highlight - shadow) / 255
+        gamma_b = shadow
+
+        buf = cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
+    else:
+        buf = input_img.copy()
+
+    if contrast != 0:
+        f = 131 * (contrast + 127) / (127 * (131 - contrast))
+        alpha_c = f
+        gamma_c = 127 * (1 - f)
+
+        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+
+    return buf
+
+
+def crop_and_get_buttons_only(input_image):
+    image_clone = input_image.copy()
+    im_height, im_width = image_clone.shape[:2]
+    mid_point = im_width / 2
+    half_scan_width = WIDTH_PERCENTAGE_OF_SCANING_AREA * im_width / 200
+    return image_clone[:, mid_point - half_scan_width: mid_point + half_scan_width]
+
+
+def stich_cropped_buttons_back(original, cropped):
+    image_clone = original.copy()
+    im_height, im_width = image_clone.shape[:2]
+    mid_point = im_width / 2
+    half_scan_width = WIDTH_PERCENTAGE_OF_SCANING_AREA * im_width / 200
+    image_clone[:, mid_point - half_scan_width: mid_point + half_scan_width] = cropped
+    return image_clone
+
+
+def get_button_keypoints(cropped_img):
+    cropped_img = apply_brightness_contrast(cropped_img, 0, 40)
+    # show_image_fit_screen(cropped_img, "Intermediate")
+    # cv2.waitKey(0)
     # Set up the detector with default parameters.
     detector = cv2.SimpleBlobDetector_create()
 
     # Detect blobs.
-    keypoints = detector.detect(grayscale_img)
+    keypoints = detector.detect(cropped_img)
     # Sort to get the top button at first
     keypoints.sort(key=lambda coord: coord.pt[1])
     return keypoints
 
 
 def draw_buttons_on_image(_keypoints, source_img):
+    print "{0} BUTTONS DETECTED".format(len(_keypoints))
+    if len(_keypoints) < 2:
+        return None
     # Draw detected blobs as red circles.
     ax, ay = _keypoints[0].pt
     bx, by = _keypoints[1].pt
-    img_out = None
     img_out = cv2.line(source_img, (int(ax), int(ay)), (int(bx), int(by)), (255, 0, 0), 4)
     cv2.drawKeypoints(img_out, _keypoints, img_out, (0, 255, 0),
                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -44,15 +94,15 @@ def calculate_button_distance_inches(keypoints):
     return real_distance
 
 
-def show_image_fit_screen(image):
+def show_image_fit_screen(image, window_name):
     im_height, im_width = image.shape[:2]
     print str(im_height) + " x " + str(im_width)
     window_height = screensize[1] * 0.8
     window_width = window_height * (im_width / float(im_height))
     print (im_height / float(im_width))
-    cv2.namedWindow('Output', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Output', int(window_width), int(window_height))
-    cv2.imshow("Output", im_with_keypoints)
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, int(window_width), int(window_height))
+    cv2.imshow(window_name, image)
 
 
 def render_instructions(image_):
@@ -80,14 +130,19 @@ def render_result(image_, inches):
 
 
 # Read image
-img_color = cv2.imread("img/shirt1.jpg", cv2.IMREAD_COLOR)
-img = cv2.imread("img/shirt1.jpg", cv2.IMREAD_GRAYSCALE)
+img_color = cv2.imread("img/" + IMAGE_FILE_NAME, cv2.IMREAD_COLOR)
 
-keypoint_list = get_button_keypoints(img)
-im_with_keypoints = draw_buttons_on_image(keypoint_list, img_color)
-inches = calculate_button_distance_inches(keypoint_list)
-# Show keypoints
-render_instructions(im_with_keypoints)
-render_result(im_with_keypoints, inches)
-show_image_fit_screen(im_with_keypoints)
-cv2.waitKey(0)
+cropped_color = crop_and_get_buttons_only(img_color)
+
+keypoint_list = get_button_keypoints(cropped_color)
+im_with_keypoints = draw_buttons_on_image(keypoint_list, cropped_color)
+if im_with_keypoints is not None:
+    stiched_final_image = stich_cropped_buttons_back(img_color, im_with_keypoints)
+    inches = calculate_button_distance_inches(keypoint_list)
+    # Show keypoints
+    render_instructions(stiched_final_image)
+    render_result(stiched_final_image, inches)
+    show_image_fit_screen(stiched_final_image, "Result")
+    cv2.waitKey(0)
+else:
+    print "DETECTION FAILED"

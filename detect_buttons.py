b75fec8a416876ae8f21f590e9a15d03eee9439e
W.H.Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2019-02-20 02:25:11 +0530

    Decide yes no and draw line between buttons

--- detect_buttons.py
+++ detect_buttons.py
@@ -4,7 +4,9 @@ import cv2
 import numpy as np
 import ctypes
 
-PIXELS_PER_CENTIMETER = 18
+# SETTINGS
+PIXELS_PER_INCH = 50.7
+TOLERENCE_INCHES = 0.2
 
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
@@ -24,17 +26,22 @@ def get_button_keypoints(grayscale_img):
 
 def draw_buttons_on_image(_keypoints, source_img):
     # Draw detected blobs as red circles.
-    return cv2.drawKeypoints(source_img, _keypoints, np.array([]), (0, 255, 0),
-                             cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    ax, ay = _keypoints[0].pt
+    bx, by = _keypoints[1].pt
+    img_out = None
+    img_out = cv2.line(source_img, (int(ax), int(ay)), (int(bx), int(by)), (255, 0, 0), 4)
+    cv2.drawKeypoints(img_out, _keypoints, img_out, (0, 255, 0),
+                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return img_out
 
 
-def calculate_button_distance(keypoints):
+def calculate_button_distance_inches(keypoints):
     topbutton_point = keypoints[0].pt
     secondbutton_point = keypoints[1].pt
     pixel_distance = secondbutton_point[1] - topbutton_point[1]
-    real_distance = pixel_distance / PIXELS_PER_CENTIMETER
-    print "Distance between first two buttons: " + "{0:.2f}".format(real_distance) + "cm"
-    print "Distance between first two buttons: " + "{0:.2f}".format(real_distance / 2.54) + "inches"
+    real_distance = pixel_distance / PIXELS_PER_INCH
+    print "Distance between first two buttons: " + "{0:.2f}".format(real_distance) + "inches"
+    return real_distance
 
 
 def show_image_fit_screen(image):
@@ -53,29 +60,34 @@ def render_instructions(image_):
     im_width, im_height = image_.shape[:2]
     cv2.rectangle(image_, (0, im_width - 40), (im_height, im_width), (0, 0, 0), cv2.FILLED,
                   lineType=8)
-    cv2.putText(image_, 'ESC TO EXIT', (0, im_width - 10), font, 1,
+    cv2.putText(image_, 'PRESS ANY KEY TO EXIT', (0, im_width - 10), font, 1,
                 (255, 255, 255),
                 3, cv2.LINE_AA)
 
 
-def render_result(image_, result_string):
+def render_result(image_, inches):
     global font
+    is_acceptable = abs(inches - 2) < TOLERENCE_INCHES
+    result_text = "Button Distance: {0:.2f}\" ACCEPTED: {1}".format(inches, "YES" if is_acceptable else "NO")
+
     im_height, im_width = image_.shape[:2]
-    cv2.rectangle(image_, (0, im_height - 80), (im_width, im_height - 40), (0, 255, 0), cv2.FILLED,
+    cv2.rectangle(image_, (0, im_height - 80), (im_width, im_height - 40),
+                  (0, 255, 0) if is_acceptable else (0, 0, 255), cv2.FILLED,
                   lineType=8)
-    cv2.putText(image_, result_string, (0, im_height - 50), font, 1,
+    cv2.putText(image_, result_text, (0, im_height - 50), font, 1,
                 (0, 0, 0),
                 3, cv2.LINE_AA)
 
 
 # Read image
+img_color = cv2.imread("img/shirt1.jpg", cv2.IMREAD_COLOR)
 img = cv2.imread("img/shirt1.jpg", cv2.IMREAD_GRAYSCALE)
 
 keypoint_list = get_button_keypoints(img)
-im_with_keypoints = draw_buttons_on_image(keypoint_list, img)
-calculate_button_distance(keypoint_list)
+im_with_keypoints = draw_buttons_on_image(keypoint_list, img_color)
+inches = calculate_button_distance_inches(keypoint_list)
 # Show keypoints
 render_instructions(im_with_keypoints)
-render_result(im_with_keypoints, "LOL")
+render_result(im_with_keypoints, inches)
 show_image_fit_screen(im_with_keypoints)
 cv2.waitKey(0)

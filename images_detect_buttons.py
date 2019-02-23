71aefdfce731d6660902317c124b717179a10c62
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2019-02-23 18:37:10 +0530

    Change image button detection width

--- images_detect_buttons.py
+++ images_detect_buttons.py
@@ -2,10 +2,10 @@ import detector_core
 
 # SETTINGS
 _settings = {
-    "IMAGE_FILE_NAME": "shirt (6).jpg",
+    "IMAGE_FILE_NAME": "shirt (10).jpg",
     "PIXELS_PER_INCH": 40,
     "TOLERANCE_INCHES": 0.2,
-    "WIDTH_PERCENTAGE_OF_SCANNING_AREA": 70
+    "WIDTH_PERCENTAGE_OF_SCANNING_AREA": 30
 }
 
 detector_core.detection_api_detect_from_image(_settings)
